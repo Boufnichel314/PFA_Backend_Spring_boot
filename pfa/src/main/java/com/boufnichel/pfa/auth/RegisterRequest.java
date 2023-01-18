ab192778499874e39877417a3daf7a3884944830
Boufnichel314 <Boufnichel314@gmail.com>
2023-01-18 09:52:07 -0800

    Add the register_request

---
+++ pfa/src/main/java/com/boufnichel/pfa/auth/RegisterRequest.java
@@ -0,0 +1,21 @@
+package com.boufnichel.pfa.auth;
+
+import com.boufnichel.pfa.user.Role;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+  private String firstname;
+  private String lastname;
+  private String email;
+  private String password;
+  private Role role;
+}
