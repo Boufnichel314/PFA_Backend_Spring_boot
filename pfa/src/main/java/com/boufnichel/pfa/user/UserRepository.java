d4d5508b63ba3153834e9f1ab63c213f7adbc6e2
Boufnichel314 <Boufnichel314@gmail.com>
2023-01-18 01:28:09 -0800

    add the UserRepo to manage the data

---
+++ pfa/src/main/java/com/boufnichel/pfa/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.boufnichel.pfa.user;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+	Optional<User> findByEmail(String email);
+}
