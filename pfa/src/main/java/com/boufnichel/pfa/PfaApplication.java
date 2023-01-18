bb4739521af4c975551c585d507cecaa180ff6e6
Boufnichel314 <Boufnichel314@gmail.com>
2023-01-18 10:27:29 -0800

    unlock the CORS to call all the APIs

--- pfa/src/main/java/com/boufnichel/pfa/PfaApplication.java
+++ pfa/src/main/java/com/boufnichel/pfa/PfaApplication.java
@@ -2,6 +2,9 @@ package com.boufnichel.pfa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PfaApplication {
@@ -9,5 +12,16 @@ public class PfaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PfaApplication.class, args);
 	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+				.allowedMethods("*")
+				.allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
+	
 
 }
