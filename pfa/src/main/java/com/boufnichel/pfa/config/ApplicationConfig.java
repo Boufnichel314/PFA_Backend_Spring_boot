5c1d3809440c8004c0516c58e38052fad0d7975f
Boufnichel314 <Boufnichel314@gmail.com>
2023-01-18 09:38:00 -0800

    implement securityFilterChain as a bean (new spring boot security config)

--- pfa/src/main/java/com/boufnichel/pfa/config/ApplicationConfig.java
+++ pfa/src/main/java/com/boufnichel/pfa/config/ApplicationConfig.java
@@ -31,7 +31,13 @@ public class ApplicationConfig {
         .orElseThrow(() -> new UsernameNotFoundException("User not found"));
   }
 
-  
+  @Bean
+  public AuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+    authProvider.setUserDetailsService(userDetailsService());
+    authProvider.setPasswordEncoder(passwordEncoder());
+    return authProvider;
+  }
 
   @Bean
   public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
