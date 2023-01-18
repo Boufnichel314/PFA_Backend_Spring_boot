1ba361d4510da46fd20cca32e5e842da73afe8a7
Boufnichel314 <Boufnichel314@gmail.com>
2023-01-18 08:51:08 -0800

    check the autorization to extract the JWT

--- pfa/src/main/java/com/boufnichel/pfa/config/JwtAuthenticationFilter.java
+++ pfa/src/main/java/com/boufnichel/pfa/config/JwtAuthenticationFilter.java
@@ -2,19 +2,40 @@ package com.boufnichel.pfa.config;
 
 import java.io.IOException;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.pfa.pfasecurity.config.JwtService;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
+import lombok.RequiredArgsConstructor;
+@Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+	
+	  
+	  private final UserDetailsService userDetailsService;
 
 	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+	protected void doFilterInternal(
+			HttpServletRequest request, 
+			HttpServletResponse response, 
+			FilterChain filterChain
+			)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		final String authHeader = request.getHeader("Authorization");
+	    final String jwt;
+	    final String userEmail;
+	    if (authHeader == null ||!authHeader.startsWith("Bearer ")) {
+	        filterChain.doFilter(request, response);
+	        return;
+	      }
+	    jwt = authHeader.substring(7);
+	    
 		
 	}
 
