f915e6b4a7d3bb30404454568ec388e6fa18e38d
Boufnichel314 <Boufnichel314@gmail.com>
2023-01-18 10:39:45 -0800

    finishing the authentification security

--- pfa/src/main/java/com/boufnichel/pfa/auth/AuthenticationController.java
+++ pfa/src/main/java/com/boufnichel/pfa/auth/AuthenticationController.java
@@ -10,10 +10,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.boufnichel.pfa.user.User;
 import com.boufnichel.pfa.user.UserRepository;
-
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -21,13 +19,38 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthenticationController {
 
+  private final AuthenticationService service;
   private final UserRepository repository;
-  
-  
+  @PostMapping("/register")
+  public ResponseEntity<AuthenticationResponse> register(
+      @RequestBody RegisterRequest request
+  ) {
+    return ResponseEntity.ok(service.register(request));
+  }
+  @PostMapping("/authenticate")
+  public ResponseEntity<AuthenticationResponse> authenticate(
+      @RequestBody AuthenticationRequest request
+  ) {
+    return ResponseEntity.ok(service.authenticate(request));
+  }
+
   //make call to get all users
   @GetMapping("/users")
   public List<User> getAllUsers() {
       return repository.findAll();
   }
-  
+
+  @PostMapping("/approve/{id}")
+  //Update user to approved with repository String
+  public String approveUser(@PathVariable int id) {
+      User user = repository.findById(id).orElseThrow();
+      user.setApproved(true);
+      repository.save(user);
+      return "User approved";
+  }
+    //get users who are not approved
+    @GetMapping("/users/notapproved")
+    public List<User> getNotApprovedUsers() {
+        return repository.findByApproved(false);
+    }
 }
--- pfa/src/main/java/com/boufnichel/pfa/user/UserRepository.java
+++ pfa/src/main/java/com/boufnichel/pfa/user/UserRepository.java
@@ -1,9 +1,10 @@
 package com.boufnichel.pfa.user;
 
+import java.util.List;
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 	Optional<User> findByEmail(String email);
+	List<User> findByApproved(boolean approved);
 }
