f92d985517e2e0bb5ac6f32a66869f2c1f404889
Boufnichel314 <Boufnichel314@gmail.com>
2023-01-18 08:43:08 -0800

    rpepare JwtFilter for checking the Tokens

---
+++ pfa/src/main/java/com/boufnichel/pfa/config/JwtAuthenticationFilter.java
@@ -0,0 +1,21 @@
+package com.boufnichel.pfa.config;
+
+import java.io.IOException;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
