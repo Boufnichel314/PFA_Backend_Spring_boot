d5a81213103119d7d10d417b7c46a83554e0c12a
Boufnichel314 <Boufnichel314@gmail.com>
2023-01-18 01:01:38 -0800

    install the spring boot project

---
+++ pfa/src/main/java/com/boufnichel/pfa/PfaApplication.java
@@ -0,0 +1,13 @@
+package com.boufnichel.pfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfaApplication.class, args);
+	}
+
+}
