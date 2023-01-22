72c6f1a7eedcd3d006bca4d90f240965ec66a78c
Boufnichel314 <Boufnichel314@gmail.com>
2023-01-22 00:49:16 -0800

    upload and download methods

--- pfa/src/main/java/com/pfa/pfasecurity/material/MaterialService.java
+++ pfa/src/main/java/com/pfa/pfasecurity/material/MaterialService.java
@@ -1,10 +1,11 @@
 package com.pfa.pfasecurity.material;
 
-
+import java.io.IOException;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class MaterialService {
@@ -12,5 +13,19 @@ public class MaterialService {
 	@Autowired
 	private MaterialRepo materialrepo;
 	
+	public String uploadImage(MultipartFile file, String name, String description) throws IOException {
+	    Material material = materialrepo.save(Material.builder()
+	            .name(name)
+	            .description(description)
+	            .image(ImageUtils.compressImage(file.getBytes()))
+	            .build());
+	    if (material != null) return "File Uploaded";
+	    else return "Error";
+	}
 	
+	public byte[] downloadImage(String fileName){
+        Optional<Material> dbImageData = materialrepo.findByName(fileName);
+        byte[] images=ImageUtils.decompressImage(dbImageData.get().getImage());
+        return images;
+    }
 }
