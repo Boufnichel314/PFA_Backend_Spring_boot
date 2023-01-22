75f409f952004ec08ebb39142f492da94041fe33
Boufnichel314 <Boufnichel314@gmail.com>
2023-01-22 00:35:01 -0800

    prepare the materialService

---
+++ pfa/src/main/java/com/pfa/pfasecurity/material/MaterialService.java
@@ -0,0 +1,16 @@
+package com.pfa.pfasecurity.material;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class MaterialService {
+	
+	@Autowired
+	private MaterialRepo materialrepo;
+	
+	
+}
