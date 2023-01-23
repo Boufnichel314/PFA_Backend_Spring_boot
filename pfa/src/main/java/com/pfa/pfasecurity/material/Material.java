e5b2f84d1701c04143091d390dbffb3243f9d53d
Boufnichel314 <Boufnichel314@gmail.com>
2023-01-23 15:38:50 -0800

    update Material Entity

--- pfa/src/main/java/com/pfa/pfasecurity/material/Material.java
+++ pfa/src/main/java/com/pfa/pfasecurity/material/Material.java
@@ -1,43 +1,31 @@
 package com.pfa.pfasecurity.material;
-import java.util.List;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
+import java.util.List;
+
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+@Entity(name = "material")
 public class Material {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
-	private String name;
-	private byte[] image;
-	private boolean isDispo = false;
-	private List<String> mots_cle;
-	private String description;
-	public Material(String name, byte[] image, String description, boolean isDispo, List<String> mots_cle) {
-		super();
-		this.name = name;
-		this.image = image;
-		this.description = description;
-		this.isDispo = isDispo;
-		this.mots_cle = mots_cle;
-	}
-	public void setIsDispo(boolean b) {
-		this.isDispo = b;
-	}
-	
-	
-	
-	
-	
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String sku;
+    private String titre;
+    private String description;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "material_id",referencedColumnName = "id")
+    private List<Image> images;
+    private String departement;
+    private boolean disponible;
+    private String tags;
+
 }
