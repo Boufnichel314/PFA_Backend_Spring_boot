4350459111fede40af14330c1dcf9405f4b56e8e
Boufnichel314 <Boufnichel314@gmail.com>
2023-01-23 00:26:09 -0800

    materials handling with id

--- pfa/src/main/java/com/pfa/pfasecurity/material/MaterialService.java
+++ pfa/src/main/java/com/pfa/pfasecurity/material/MaterialService.java
@@ -1,12 +1,16 @@
 package com.pfa.pfasecurity.material;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 
 @Service
 public class MaterialService {
@@ -38,5 +42,29 @@ public class MaterialService {
 	    }
 	    return materials;
 	}
+	
+	@Transactional
+	public void reserveMaterial(int id) {
+	    Material material = materialrepo.findById(id).orElseThrow(() -> new EntityNotFoundException("Material not found with id: " + id));
+	    if (material.isDispo()) {
+	        material.setIsDispo(false);
+	        materialrepo.save(material);
+	    } else {
+	        throw new IllegalStateException("Material is not available for reservation");
+	    }
+	}
+	
+	public void addKeyword(int id, String keyword) {
+        Material material = materialrepo.findById(id)
+            .orElseThrow(() -> new EntityNotFoundException("Material not found"));
+        List<String> keywords = material.getMots_cle();
+        if(keywords == null){
+            keywords = new ArrayList<>();
+        }
+        keywords.add(keyword);
+        material.setMots_cle(keywords);
+        materialrepo.save(material);
+    }
+
 
 }
