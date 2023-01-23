7f70576b928fa773a9999740190c12af62645e46
Boufnichel314 <Boufnichel314@gmail.com>
2023-01-23 00:19:18 -0800

    entity update

--- pfa/src/main/java/com/pfa/pfasecurity/material/Material.java
+++ pfa/src/main/java/com/pfa/pfasecurity/material/Material.java
@@ -1,4 +1,6 @@
 package com.pfa.pfasecurity.material;
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,12 +21,19 @@ public class Material {
 	private int id;
 	private String name;
 	private byte[] image;
+	private boolean isDispo = false;
+	private List<String> mots_cle;
 	private String description;
-	public Material(String name, byte[] image, String description) {
+	public Material(String name, byte[] image, String description, boolean isDispo, List<String> mots_cle) {
 		super();
 		this.name = name;
 		this.image = image;
 		this.description = description;
+		this.isDispo = isDispo;
+		this.mots_cle = mots_cle;
+	}
+	public void setIsDispo(boolean b) {
+		this.isDispo = b;
 	}
 	
 	
