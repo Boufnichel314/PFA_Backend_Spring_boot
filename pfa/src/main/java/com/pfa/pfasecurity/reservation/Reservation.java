6318a1a4200f7fff7a7f8a9ab3a56b1b669179bb
Boufnichel314 <Boufnichel314@gmail.com>
2023-01-25 00:36:29 -0800

    Reservation entity

---
+++ pfa/src/main/java/com/pfa/pfasecurity/reservation/Reservation.java
@@ -0,0 +1,63 @@
+package com.pfa.pfasecurity.reservation;
+
+import java.util.Date;
+
+import com.pfa.pfasecurity.material.Material;
+import com.pfa.pfasecurity.user.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Reservation {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private Date dueDate;
+    private Date reservationDate;
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "material_id", referencedColumnName = "id")
+    private Material material;
+    
+    
+    
+	public Reservation() {
+		super();
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Date getDueDate() {
+		return dueDate;
+	}
+	public void setDueDate(Date dueDate) {
+		this.dueDate = dueDate;
+	}
+	public Date getReservationDate() {
+		return reservationDate;
+	}
+	public void setReservationDate(Date reservationDate) {
+		this.reservationDate = reservationDate;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public Material getMaterial() {
+		return material;
+	}
+	public void setMaterial(Material material) {
+		this.material = material;
+	}
+}
---
+++ pfa/src/main/java/com/pfa/pfasecurity/reservation/reservationRepository.java
@@ -0,0 +1,7 @@
+package com.pfa.pfasecurity.reservation;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface reservationRepository extends JpaRepository<Reservation, Integer> {
+
+}
