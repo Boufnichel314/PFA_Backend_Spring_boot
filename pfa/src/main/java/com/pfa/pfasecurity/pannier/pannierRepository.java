8592f9e7a5a9184cbdf5c5aab3dd9060f25894f3
Boufnichel314 <Boufnichel314@gmail.com>
2023-01-28 14:17:04 -0800

    prepare deleting

--- pfa/src/main/java/com/pfa/pfasecurity/pannier/pannierRepository.java
+++ pfa/src/main/java/com/pfa/pfasecurity/pannier/pannierRepository.java
@@ -7,4 +7,8 @@ import org.springframework.data.jpa.repository.Query;
 
 import com.pfa.pfasecurity.user.User;
 
-public interface pannierRepository extends JpaRepository<Pannier, Integer> {    }
+public interface pannierRepository extends JpaRepository<Pannier, Integer> {
+
+	public void deleteByUser(User user);    
+	
+}
