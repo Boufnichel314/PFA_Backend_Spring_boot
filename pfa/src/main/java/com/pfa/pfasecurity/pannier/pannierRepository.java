952588c5cf561c85b1e73742466ddc87a6932589
Boufnichel314 <Boufnichel314@gmail.com>
2023-01-28 04:59:20 -0800

    Repo interface

---
+++ pfa/src/main/java/com/pfa/pfasecurity/pannier/pannierRepository.java
@@ -0,0 +1,10 @@
+package com.pfa.pfasecurity.pannier;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.pfa.pfasecurity.user.User;
+
+public interface pannierRepository extends JpaRepository<Pannier, Integer> {    }
