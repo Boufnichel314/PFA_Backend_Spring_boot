b2dd67668ad6cfc146113ba3015e6830c265a2a9
Boufnichel314 <Boufnichel314@gmail.com>
2023-01-28 05:00:58 -0800

    what users will insert

---
+++ pfa/src/main/java/com/pfa/pfasecurity/pannier/PannierDto.java
@@ -0,0 +1,23 @@
+package com.pfa.pfasecurity.pannier;
+
+import java.util.List;
+
+public class PannierDto {
+    private Integer userId;
+    private Integer materialId;
+    // getters and setters for userId and materialIds
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public Integer getMaterialId() {
+		return materialId;
+	}
+	public void setMaterialIds(Integer materialIds) {
+		this.materialId = materialIds;
+	}
+    
+    
+}
