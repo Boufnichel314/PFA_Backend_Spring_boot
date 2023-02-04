447e700a79fa1d106f9c193cdcc37848798095f7
Boufnichel314 <Boufnichel314@gmail.com>
2023-02-04 04:58:14 -0800

    prepare the Material Controller

---
+++ pfa/src/main/java/com/pfa/pfasecurity/material/MaterialController.java
@@ -0,0 +1,13 @@
+package com.pfa.pfasecurity.material;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class MaterialController {
+	
+}
