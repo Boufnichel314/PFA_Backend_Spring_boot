ae3ea2c83be229fcb7aa4bc8cee87c6e9a444035
Boufnichel314 <Boufnichel314@gmail.com>
2023-02-04 05:11:41 -0800

    prepare Reservation controller

---
+++ pfa/src/main/java/com/pfa/pfasecurity/reservation/ReservationController.java
@@ -0,0 +1,11 @@
+package com.pfa.pfasecurity.reservation;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class ReservationController {
+
+}
