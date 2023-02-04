2de2714143fa68eaf9e9e7f54b4753fddadffd87
Boufnichel314 <Boufnichel314@gmail.com>
2023-02-04 05:15:48 -0800

    prepare User Controller

--- pfa/src/main/java/com/pfa/pfasecurity/user/UserController.java
+++ pfa/src/main/java/com/pfa/pfasecurity/user/UserController.java
@@ -1,5 +1,16 @@
 package com.pfa.pfasecurity.user;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pfa.pfasecurity.auth.AuthenticationService;
+import com.pfa.pfasecurity.material.MaterialRepo;
+import com.pfa.pfasecurity.pannier.pannierRepository;
+import com.pfa.pfasecurity.reservation.reservationRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
 public class UserController {
 
 }
