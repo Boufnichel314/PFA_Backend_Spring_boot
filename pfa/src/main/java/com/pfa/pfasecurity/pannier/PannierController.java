94d69eb0108edb111b440dffecb45c106cbe5e9d
Boufnichel314 <Boufnichel314@gmail.com>
2023-02-04 05:05:40 -0800

    prepare pannier controller

---
+++ pfa/src/main/java/com/pfa/pfasecurity/pannier/PannierController.java
@@ -0,0 +1,13 @@
+package com.pfa.pfasecurity.pannier;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pfa.pfasecurity.material.MaterialRepo;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class PannierController {
+
+}
